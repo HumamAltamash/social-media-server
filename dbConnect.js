8caa5a49ea0211f8ee4b5d5e78eb85887da99895
HumamAltamash <humamaltamash93@gmail.com>
2023-01-21 14:00:00 +0530

    all initial features

---
+++ dbConnect.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    const mongoUri =
+        "mongodb+srv://humam:AV0OLRZxjKZq2Asg@cluster0.r7wcq5w.mongodb.net/?retryWrites=true&w=majority";
+    try {
+        const connect = await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`MongoDB connected: ${connect.connection.host}`)
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
